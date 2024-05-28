2fab4b9ba7b63cff135df6c608c62c0fda0e1a24
surpog17 <surafelkifle17@gmail.com>
2024-05-28 16:55:29 +0300

    updating the docker file

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Landing from "./pages/Landing";
 import Main from "./pages/Main";
 
 function App() {
-  const token = false;
+  const token = true;
   return (
     <div className="font-sans antialiased text-gray-900">
       {!token ? <Landing /> : <Main token={token} />}
